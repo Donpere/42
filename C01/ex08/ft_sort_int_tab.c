eaa79b45b017ecbe51689911cc6790c85f562edd
Dominique Marion <domarion@e2r10p10.clusters.42paris.fr>
2022-09-27 15:17:23 +0200

    all exo : clean header - ex08 running

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,35 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/27 15:11:36 by domarion          #+#    #+#             */
-/*   Updated: 2022/09/27 15:11:45 by domarion         ###   ########.fr       */
+/*   Created: 2022/09/27 15:13:47 by domarion          #+#    #+#             */
+/*   Updated: 2022/09/27 15:13:54 by domarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
 #include <stdio.h>
 
-int ft_strlen(char *str)
+void ft_rev_int_tab(int *tab, int size)
 {
-    int i;
-    i = 0;
+	int tmp;
+	int i;
 
-    while (str[i] != '\0')
-    {
-        i++;
-    }
-    return (i);
+	i = 0;
+	size = size - 1;
+
+	while (i < size)
+	{
+		tmp = tab[size];
+		tab[size] = tab[i];
+		tab[i] = tmp; 
+		i++;
+		size--;
+	}
 }
 
 int main()
 {
-    int y = 0;
-    char    str[] = "lalala";
-    y = ft_strlen(str);
-    printf("%d", y);
-    return 0;
+	int    array[5] = {8, 4, 9, 3, 1};
+	int big = 5;
+	int i = 0 - 1;
+
+	while (++i < big)
+	{
+			printf("%d", array[i]);
+	}
+			printf("\n");
+
+	ft_rev_int_tab(array, big);
+
+	i = 0 - 1;
+
+	while (++i < big)
+			printf("%d", array[i]);
+
+	return 0;
 }

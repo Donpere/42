0e3cf7f3021540c0cac0b141c8bf4c86a3838ee7
Dominique Marion <domarion@e2r10p10.clusters.42paris.fr>
2022-09-27 14:56:35 +0200

    add C01 ex07 without norm & flag

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/24 15:38:50 by domarion          #+#    #+#             */
+/*   Updated: 2022/09/24 15:39:17 by domarion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+void ft_rev_int_tab(int *tab, int size)
+{
+	int tmp;
+	int i;
+
+	i = 0;
+	size = size - 1;
+
+	while (i < size)
+	{
+		tmp = tab[size];
+		tab[size] = tab[i];
+		tab[i] = tmp; 
+		i++;
+		size--;
+	}
+}
+
+int main()
+{
+	int    array[5] = {8, 4, 9, 3, 1};
+	int big = 5;
+	int i = 0 - 1;
+
+	while (++i < big)
+	{
+			printf("%d", array[i]);
+	}
+			printf("\n");
+
+	ft_rev_int_tab(array, big);
+
+	i = 0 - 1;
+
+	while (++i < big)
+			printf("%d", array[i]);
+
+	return 0;
+}

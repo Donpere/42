d5778a1557f37c1bd028f508a7e5bc6db394c2dc
Dominique Marion <domarion@e2r3p9.clusters.42paris.fr>
2022-09-28 19:27:21 +0200

    ex01 fini & clean - .out suppress

--- C02/ex01/ft_strncpy.c
+++ C02/ex01/ft_strncpy.c
@@ -10,4 +10,50 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+- La fonction strNcpy() copie la chaîne pointée par src 
+(y compris l'octet nul « \0 » final) dans la chaîne pointée par dest. 
+- Les deux chaînes ne doivent pas se chevaucher. 
+- La chaîne dest doit être assez grande pour accueillir la copie.
+- Les fonctions strcpy() et strncpy() 
+renvoient un pointeur sur la chaîne destination dest.  
 
+- IDEM sauf que seuls les n premiers octets de src sont copiés.
+*/
+//#include <stdio.h>
+//#include <unistd.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+	dest[i] = '\0';
+	i++;
+	}
+	return (dest);
+}
+
+/*int main()
+{
+	unsigned int n = 6;
+	char	dest[] = "1123344";
+	char	src[] = "Toto";
+
+
+printf("n = %d\n, dest : %s \n src : %s \n", n, dest, src);
+
+ft_strncpy(dest, src, n);
+
+printf("dest : %s \n src : %s \n", dest, src);
+
+return 0;
+}
+*/

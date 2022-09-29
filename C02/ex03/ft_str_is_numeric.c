8c48cf65308d2f3929765d910dc4865436882327
Dominique Marion <domarion@e2r6p9.clusters.42paris.fr>
2022-09-29 10:15:30 +0200

    C02 ex03 fin & clean

---
+++ C02/ex03/ft_str_is_numeric.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_str_is_numeric.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/29 09:56:41 by domarion          #+#    #+#             */
+/*   Updated: 2022/09/29 09:56:50 by domarion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_str_is_numeric(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (str[0] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 48 && str[i] <= 57)
+			i++;
+		else
+			return (0);
+	}
+	return (1);
+}
+
+/*int	main ()
+{
+	int	a = 0;
+	char	string[] = "345m9867";
+
+	a = ft_str_is_numeric(string);
+	printf("%d \n", a);
+	return (0);
+}*/

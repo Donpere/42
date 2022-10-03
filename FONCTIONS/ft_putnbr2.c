0abdb556a2ecb1195f84ac08b9bd8873410614d9
Dominique Marion <domarion@e2r4p13.clusters.42paris.fr>
2022-10-03 09:29:47 +0200

    putnbr2 2

--- FONCTIONS/ft_putnbr2.c
+++ FONCTIONS/ft_putnbr2.c
@@ -1,47 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/03 09:27:31 by domarion          #+#    #+#             */
+/*   Updated: 2022/10/03 09:27:41 by domarion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 
-void ft_putstr(char *str)
+void	ft_putstr(char *str)
 {
-	int i;
+	int	i;
 
 	i = 0;
-	while(str[i] != '\0')
+	while (str[i] != '\0')
 	{
 		ft_putchar(str[i]);
 		i++;
 	}
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-unsigned n;
+	unsigned int	n;
 
 n = nb;
 	if (nb < 0)
 	{
-		n = -nb;
-		ft_putchar('\0');
+		nb = -n;
+		ft_putchar('-');
 		ft_putnbr(nb);
 	}
-
-	else if (nb >= 10)
+	else if (nb > 9)
 	{
 		ft_putnbr(nb / 10);
 		ft_putnbr(nb % 10);
 	}
-
 	else
 	{
 		ft_putchar(nb + '0');
 	}
 }
 
-int main()
+int	main()
 {
 	ft_putnbr(-45);
 	return (0);

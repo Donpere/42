23be16534199e4dbbba6954a3fae8acca1f39465
Dominique MARION <domarion@e2r2p6.clusters.42paris.fr>
2022-11-23 19:54:14 +0100

    CHEVAUXdeCOURSE v1

--- CODINGgame/CHEVAUXdeCOURSE.c
+++ CODINGgame/CHEVAUXdeCOURSE.c
@@ -11,37 +11,60 @@
 int main()
 {
     int N;
-    int tmp = 0;
     int answer = 0;
     int *tab = NULL;
     scanf("%d", &N);
     tab = (int *) malloc( N * sizeof(int) );
+   
+    //verif malloc
+
+// remp tab
     for (int i = 0; i < N; i++) 
         {
             int pi;
             scanf("%d", &pi);
             tab[i] = pi;
         }
-
+//print tab
     for (int j = 0; j < N; j++)
         {
             printf("%d", tab[j]);
-        } 
+        }  
     printf("\n");
 
-        for (int j = 0; j < N; j++)
+//remp tab_ecart
+    int *tab_ecart = NULL;
+    tab_ecart = (int *) malloc( N * N * sizeof(int) );
+
+    int j = 0;
+    int k = 0;
+        for (int i = 0; i < N; i++)
             {
                 for (int j = 0; j < N; j++)
                 {
-                    tmp = abs(tab[i]-tab[j])
-                    if (tmp < answer)
-                        answer = tmp; 
+                    if (i != j)
+                        tab_ecart[k] = abs(tab[i]-tab[j]);
+                        k++;
                 } 
             }
+//trouver le plus petit ecart dans tab 
+
+
+//print tab
+    for (int j = 0; j < N; j++)
+        {
+            printf("%d", tab_ecart[j]);
+        }  
+    printf("\n");
 
 
     // Write an answer using printf(). DON'T FORGET THE TRAILING \n
     // To debug: fprintf(stderr, "Debug messages...\n");
+
+    // free des malloc
+    free(tab);
+    free(tab_ecart);
+
 
     printf("answer\n");
 

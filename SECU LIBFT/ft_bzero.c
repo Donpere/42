9669be4891d64049d8769f3ff84740977e838c2e
Dominique MARION <domarion@e2r2p4.clusters.42paris.fr>
2022-11-26 10:53:44 +0100

    maj + copy SECU LIBFT

---
+++ SECU LIBFT/ft_bzero.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/20 12:05:06 by domarion          #+#    #+#             */
+/*   Updated: 2022/11/20 12:05:13 by domarion         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <strings.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// void ft_bzero(void *s, size_t n);
+
+// int main()
+// {
+
+
+// 	return (0);
+// }
+
+void ft_bzero(void *s, size_t n)
+{
+	char *set = s;
+	size_t i;
+	i = 0;
+	while (i < n)
+	{
+		set[i] = 0;
+		i++;
+	}
+	
+}

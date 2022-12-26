d4974f8be9dad6c007061a0b2981bafa1f07b881
Dominique MARION <domarion@e1r11p19.clusters.42paris.fr>
2022-12-26 19:47:02 +0100

    add cours Olivier Lapeyre

---
+++ CoursLangageCJacquesOlivierLapeyre/U1C2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+	char* a;
+	a = "salut";
+
+	printf("%s\n", a);
+
+		a = "comment va ?";
+
+	printf("%s\n", a);
+
+	int b = 5;
+	printf("%.1f\n", (float)b / 2);
+
+	printf("%c\n", 65);
+
+	return 0;
+}

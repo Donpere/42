21cff1aed44a8122950ef07a2586b5e35b43f4a6
Dominique MARION <domarion@e1r11p19.clusters.42paris.fr>
2022-12-27 17:58:37 +0100

    React Form

--- ReactTuto/tutoreact/src/Page.jsx
+++ ReactTuto/tutoreact/src/Page.jsx
@@ -4,16 +4,17 @@ import Home from './Home';
 import Form from './Form';
 
 function Page(){
+    const [isLogged, setIsLogged]=useState(false)
     return (
         <div>
-            <Form/>
-
-
+            {
+                isLogged 
+                ? <Home/>
+                : <Form isLogged={isLogged} setIsLogged={setIsLogged}/>
+            }
 
         </div>
 
     )
-
-
 }
 export default Page;
--- ReactTuto/tutoreact/src/Welcome.jsx
+++ ReactTuto/tutoreact/src/Welcome.jsx
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 import './App.css';
 
 function Welcome(props) {
-    return <div className="welcome">Bonjour, {props.name}</div>;
-  }
-  export default Welcome;
+    return (<div className="welcome">
+        Bonjour, {props.name}
+    </div>);
+}
+export default Welcome;

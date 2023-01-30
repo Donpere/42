8342d1f6fd7e241aebbbbe21e059bbf5996de07f
Dominique MARION <domarion@e1r4p13.clusters.42paris.fr>
2023-01-30 12:02:34 +0100

    rev libft

--- libftBisRevision/Lvl2Revision/ft_atoi.c
+++ libftBisRevision/Lvl2Revision/ft_atoi.c
@@ -5,13 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: domarion <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/06 15:42:08 by domarion          #+#    #+#             */
-/*   Updated: 2023/01/06 15:42:25 by domarion         ###   ########.fr       */
+/*   Created: 2023/01/30 11:05:59 by domarion          #+#    #+#             */
+/*   Updated: 2023/01/30 11:06:04 by domarion         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "libft.h"
-#include <stdio.h>
+#include "libft.h"
 
 int	ft_atoi(const char *nptr)
 {
@@ -24,30 +23,16 @@ int	ft_atoi(const char *nptr)
 	rslt = 0;
 	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
 		i++;
-	
-		printf("%d, %d, %d\n", i, neg, rslt);
-	while (nptr[i] == '+' || nptr[i] == '-')
+	if (nptr[i] == '+' || nptr[i] == '-')
 	{
 		if (nptr[i] == '-')
 			neg = neg * -1;
 		i++;
 	}
-		printf("%d, %d, %d\n", i, neg, rslt);
-
 	while (nptr[i] != '\0' && nptr[i] >= '0' && nptr[i] <= '9')
 	{
 		rslt = rslt * 10 + nptr[i] - 48;
 		i++;
 	}
-		printf("%d, %d, %d\n", i, neg, rslt);
-		printf("Good programm\n");
-
 	return (rslt * neg);
-}
-
-int	main(void)
-{
-	char str[500] = "                                ---+--+123dsfkjbdf";
-	printf("%d\n", ft_atoi(str));
-	return (0);
 }

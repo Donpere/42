795f1825409b2c9e458e7a2ba01bc236817b8365
Dominique MARION <domarion@e3r5p13.clusters.42paris.fr>
2023-04-17 16:49:48 +0200

    add dossier HTML DOC

---
+++ CPP/OpenClasseRoomCPP/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+// cette ligne permet de s'affranchir de taper std:: pour chaque commande
+// ainsi "std::cout" devient "cout"
+using namespace std;
+
+int main()
+{
+	cout << "Hello world !" << endl;
+		cout << "/n" << endl;
+
+	return 0;
+}

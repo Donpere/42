c2692c312bf87bdb2282e215c501fad6c1d62324
Dominique MARION <domarion@e3r5p12.clusters.42paris.fr>
2023-05-10 18:33:27 +0200

    update MATCHAtest/FORMULAIREdocker HTML + CSS

---
+++ startSession.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Démarrer Discord
+discord &
+
+# Démarrer Visual Studio Code
+code &
+
+# Démarrer l'explorateur de fichiers
+nautilus &
+
+# Démarrer le terminal
+gnome-terminal &
+
+# Démarrer LibreOffice Writer
+libreoffice --writer &
+
+# Démarrer Chrome
+google-chrome &

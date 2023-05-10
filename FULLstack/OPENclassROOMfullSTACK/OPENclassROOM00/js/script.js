00c86bd5f847f3345aa164eaf8122e93b96f1d57
Dominique MARION <domarion@e3r5p12.clusters.42paris.fr>
2023-05-10 16:24:45 +0200

    add doc formulaires + lexique HTML CSS JS

---
+++ FULLstack/OPENclassROOMfullSTACK/OPENclassROOM00/js/script.js
@@ -0,0 +1,15 @@
+// Create your variables here
+// ==========================================
+
+var numberOfSeasons = 6;
+var numberOfEpisodes = 12;
+
+// ==========================================
+
+let paragraph = document.querySelector('#info');
+paragraph.innerText = `${numberOfSeasons} seasons, ${numberOfEpisodes} episodes per season`
+
+
+
+
+

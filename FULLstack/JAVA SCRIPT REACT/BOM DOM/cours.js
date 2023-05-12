070c960df2f1ed26b5a8e8562245324aae905abb
Dominique MARION <domarion@e3r5p14.clusters.42paris.fr>
2023-05-12 18:20:20 +0200

    transfer fullStack

--- FULLstack/JAVA SCRIPT REACT/BOM DOM/cours.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('p1').innerHTML =
-    'Taille de la fenêtre (ext) : ' + window.outerWidth + '*' + window.outerHeight;
-
-document.getElementById('p2').innerHTML = 
-    'Taille de la fenêtre (int) : ' + window.innerWidth + '*' + window.innerHeight;
